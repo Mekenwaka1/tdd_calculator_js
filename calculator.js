e030831345a2dda3d486caacea077e76e7bc63b4
Osemeke Nwaka <o.ch.nwaka@gmail.com>
2019-09-08 15:12:28 -0400

    Created final exercise

--- calculator.js
+++ calculator.js
@@ -1,10 +1,17 @@
-function add() {
+function add(number1, number2) {
+  return number1 + number2;
 }
 
-function subtract() {
+function subtract(number1, number2) {
+  return number1 - number2;
 }
 
-function sum() {
+function sum(listOfNumbers) {
+  let total = 0;
+  listOfNumbers.forEach(number => {
+    total += number;
+  });
+  return total;
 }
 
-module.exports = {add, subtract, sum}
+module.exports = {add, subtract, sum};
--- calculator.test.js
+++ calculator.test.js
@@ -1,49 +1,49 @@
-const {add, subtract, sum} = require('./calculator.js');
+const {add, subtract, sum} = require("./calculator.js");
 
-test('adds 0 and 0', () => {
+test("adds 0 and 0", () => {
   const result = add(0, 0);
 
   expect(result).toEqual(0);
 });
 
-test('adds 2 and 2', () => {
+test("adds 2 and 2", () => {
   const result = add(2, 2);
 
   expect(result).toEqual(4);
 });
 
-test('adds positive numbers', () => {
+test("adds positive numbers", () => {
   const result = add(2, 6);
 
   expect(result).toEqual(8);
 });
 
-test('subtracts numbers', () => {
+test("subtracts numbers", () => {
   const result = subtract(10, 4);
 
   expect(result).toEqual(6);
 });
 
-test('computes sum of empty array', () => {
+test("computes sum of empty array", () => {
   const result = sum([]);
 
   expect(result).toEqual(0);
 });
 
-test('computes sum of single number', () => {
+test("computes sum of single number", () => {
   const result = sum([7]);
 
   expect(result).toEqual(7);
 });
 
-test('computes sum of two numbers', () => {
+test("computes sum of two numbers", () => {
   const result = sum([7, 11]);
 
   expect(result).toEqual(18);
 });
 
-test('computes sum of many numbers', () => {
-  const result = sum([1,3,5,7,9]);
+test("computes sum of many numbers", () => {
+  const result = sum([1, 3, 5, 7, 9]);
 
   expect(result).toEqual(25);
 });
